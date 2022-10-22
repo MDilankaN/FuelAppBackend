8bb033ca4e7e3a1cd8e764ac6abc453fa3a3a08d
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2022-10-22 22:23:41 +0530

    Database config class files

---
+++ FuelAppBackend/Models/DatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace FuelAppBackend.Models
+{
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        public string CollectionName { get; set; } = string.Empty;
+        public string ConnectionString { get; set; } = String.Empty;
+        public string DatabaseName { get; set; } = String.Empty ;
+    }
+}
