34f7259a3f202fb131edf7080d0bfc1b23b7915e
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2022-10-22 22:20:18 +0530

    StationModel

---
+++ FuelAppBackend/Models/Stations.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FuelAppBackend.Models
+{
+    [BsonIgnoreExtraElements]
+    public class Stations
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = String.Empty;
+        [BsonElement("stationname")]
+        public string StationName { get; set; } = String.Empty;
+        [BsonElement("address")]
+        public string Address { get; set; } = String.Empty;
+        [BsonElement("telephone")] public string Telephone { get; set; } = String.Empty;
+        [BsonElement("opentime")] public string OpenTime { get; set; } = String.Empty;
+        [BsonElement("closetime")] public string CloseTime { get; set; } = String.Empty;
+        [BsonElement("imageurl")] public string ImageURL { get; set; } = String.Empty;
+        [BsonElement("fuelstatus")] public bool FuelStatus { get; set; }
+        [BsonElement("noofpumps")] public int NoOfPumps { get; set; }
+    }
+}
