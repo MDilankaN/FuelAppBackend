3d721fbcab7dcd34892c3db6a273b3bd25896865
taneesha97 <taneeshaayeshmanthi97@gmail.com>
2022-10-24 13:55:06 +0530

    added user interface

---
+++ FuelAppBackend/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using FuelAppBackend.Models;
+
+namespace FuelAppBackend.Services
+{
+    public class IUserService
+    {
+        public List<User> GetUsers();
+        public User Get(String id);
+        public User Create(User user);
+        public void Update(String id, User user);
+        public void Remove(string id);
+    }
+}
---
+++ FuelAppBackend/Services/UserService.cs
@@ -0,0 +1,11 @@
+﻿using FuelAppBackend.Models;
+using MongoDB.Driver;
+using static System.Collections.Specialized.BitVector32;
+
+namespace FuelAppBackend.Services
+{
+    public class UserService
+    {
+
+    }
+}
