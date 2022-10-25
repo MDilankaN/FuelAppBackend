f614cbd29d6287c6996d7c540f8b586eeaa2af1e
taneesha97 <taneeshaayeshmanthi97@gmail.com>
2022-10-25 18:21:07 +0530

    add contactus model

---
+++ FuelAppBackend/Models/ContactUs.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace FuelAppBackend.Models
+{
+    public class ContactUs
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = String.Empty;
+        [BsonElement("name")]
+        public string Name { get; set; } = String.Empty;
+        [BsonElement("email")]
+        public string Email { get; set; } = String.Empty;
+        [BsonElement("subject")] public string Subject { get; set; } = String.Empty;
+        [BsonElement("message")] public string Message { get; set; } = String.Empty;
+       
+    }
+}
