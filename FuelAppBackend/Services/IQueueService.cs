8c44b675f105b494146a05f316d0f86e296b8c63
MDilankaN <68751531+MDilankaN@users.noreply.github.com>
2022-10-26 16:57:14 +0530

    updated getby name method

--- FuelAppBackend/Services/IQueueService.cs
+++ FuelAppBackend/Services/IQueueService.cs
@@ -5,7 +5,8 @@ namespace FuelAppBackend.Services
     public interface IQueueService
     {
         public List<Queue> GetQueues();
-        public Queue Get(String queueName);
+        public Queue GetByName(String queueName);
+        public Queue Get(String id);
         public Queue Create(Queue queue);
         public void Update(String id, Queue queue);
         public void Remove(string id);
