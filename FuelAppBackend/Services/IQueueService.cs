a26468f55ac305b699c7daf6df94c8cbebfe30e2
Sithpavan Giridaran <68677837+Sidpavn@users.noreply.github.com>
2022-10-26 13:18:20 +0530

    change id to queuename

--- FuelAppBackend/Services/IQueueService.cs
+++ FuelAppBackend/Services/IQueueService.cs
@@ -5,7 +5,7 @@ namespace FuelAppBackend.Services
     public interface IQueueService
     {
         public List<Queue> GetQueues();
-        public Queue Get(String id);
+        public Queue Get(String queueName);
         public Queue Create(Queue queue);
         public void Update(String id, Queue queue);
         public void Remove(string id);
